41be595c756d92b929829293ce7f31003e1aabc3
Ellen Galperin <egalperi@EGALPERIN.local>
2016-02-08 21:39:30 -0500

    basic setup

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 
-// Set up a URL route
-app.get("/", function(req, res) {
- res.send("Heroku Demo!");
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.send("hello world");
 });
 
-// bind the app to listen for connections on a specified port
-var port = process.env.PORT || 3000;
-app.listen(port);
-
-// Render some console log output
-console.log("Listening on port " + port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
